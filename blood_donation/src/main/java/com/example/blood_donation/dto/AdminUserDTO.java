1ec4c6ea94d9018957c67254daaf3aa148018934
gphu1332 <gphu133@gmail.com>
2025-06-19 17:34:06 +0700

    add User API for Admin
    add User API for Admin

---
+++ blood_donation/src/main/java/com/example/blood_donation/dto/AdminUserDTO.java
@@ -0,0 +1,18 @@
+package com.example.blood_donation.dto;
+
+import com.example.blood_donation.enums.Gender;
+import com.example.blood_donation.enums.Role;
+import com.example.blood_donation.enums.TypeBlood;
+import lombok.Data;
+
+@Data
+public class AdminUserDTO {
+    private String fullName;
+    private String email;
+    private String phone;
+    private String address;
+    private String cccd;
+    private Gender gender;
+    private TypeBlood typeBlood;
+    private Role role;
+}
