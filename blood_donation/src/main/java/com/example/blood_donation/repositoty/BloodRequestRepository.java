b7e806575551d8b5e7d4c48a64cb03180ef57a07
NguyenThiXuanGiang <nguyenxuangiangmt@gmail.com>
2025-06-22 18:09:37 +0700

    delete bloodrequestpriority

--- blood_donation/src/main/java/com/example/blood_donation/repositoty/BloodRequestRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.blood_donation.repositoty;
-
-import com.example.blood_donation.entity.BloodRequest;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BloodRequestRepository extends JpaRepository<BloodRequest, Integer> {
-}
