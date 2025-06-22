49f2683377818195a76b40777c7a7cfeccc16f06
NguyenThiXuanGiang <nguyenxuangiangmt@gmail.com>
2025-06-22 20:46:19 +0700

    create CRUD manage certificate

---
+++ blood_donation/src/main/java/com/example/blood_donation/service/CertificateService.java
@@ -0,0 +1,26 @@
+package com.example.blood_donation.service;
+
+import com.example.blood_donation.entity.Certificate;
+import com.example.blood_donation.repositoty.CertificateRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.config.ConfigDataResourceNotFoundException;
+
+import java.util.List;
+
+public class CertificateService {
+    @Autowired
+    private CertificateRepository certificateRepository;
+    public List<Certificate> getAllCertificates() {
+        return certificateRepository.findAll();
+    }
+    public Certificate getCertificateById(Long id) {
+        return certificateRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Certificate not found with id: " + id));
+    }
+    public Certificate createCertificate(Certificate certificate) {
+        return certificateRepository.save(certificate);
+    }
+    public Certificate updateCertificate(Long id, Certificate updateCertificate) {
+        Certificate existing = getCertificateById();
+    }
+}
