55081126eff0b21852db2e39be197e71cd2fa8cd
NguyenThiXuanGiang <nguyenxuangiangmt@gmail.com>
2025-07-13 23:02:51 +0700

    create donationDetailAPI

---
+++ blood_donation/src/main/java/com/example/blood_donation/dto/CreateDonationDetailDTO.java
@@ -0,0 +1,14 @@
+package com.example.blood_donation.dto;
+
+import lombok.Data;
+
+import java.time.LocalDate;
+@Data
+public class CreateDonationDetailDTO {
+    private Integer donAmount;
+    private LocalDate donDate;
+    private Long bloodType;
+    private Long appointmentId;
+    private Long memberId;
+    private Long staffId;
+}
---
+++ blood_donation/src/main/java/com/example/blood_donation/service/DonationDetailService.java
@@ -0,0 +1,83 @@
+package com.example.blood_donation.service;
+
+import com.example.blood_donation.dto.CreateDonationDetailDTO;
+import com.example.blood_donation.dto.DonationDetailDTO;
+import com.example.blood_donation.entity.DonationDetail;
+import com.example.blood_donation.entity.Member;
+import com.example.blood_donation.repositoty.*;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class DonationDetailService {
+    @Autowired
+    DonationDetailRepository donationDetailRepository;
+    @Autowired
+    AppointmentRepository appointmentRepository;
+    @Autowired
+    MemberRepository memberRepository;
+    @Autowired
+    StaffRepository staffRepository;
+    @Autowired
+    BloodTypeRepository bloodTypeRepository;
+
+    public DonationDetailDTO create(CreateDonationDetailDTO dto) {
+        DonationDetail donation = new DonationDetail();
+        donation.setDonAmount(dto.getDonAmount());
+        donation.setDonDate(dto.getDonDate());
+        donation.setBloodType(bloodTypeRepository.findById(dto.getBloodType())
+                .orElseThrow(() -> new EntityNotFoundException("Blood type not found")));
+        donation.setAppointment(appointmentRepository.findById(dto.getAppointmentId())
+                .orElseThrow(() -> new EntityNotFoundException("Appointment not found")));
+        donation.setMember((Member) memberRepository.findById(dto.getMemberId())
+                .orElseThrow(() -> new RuntimeException("Member not found")));
+        donation.setStaff(staffRepository.findById(dto.getStaffId())
+                .orElseThrow(() -> new EntityNotFoundException("Staff not found")));
+        return mapToDTO(donationDetailRepository.save(donation));
+    }
+    public DonationDetailDTO getById(Long id) {
+        DonationDetail d = donationDetailRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Donation not found"));
+        return mapToDTO(d);
+    }
+    public List<DonationDetailDTO> getAll() {
+        return donationDetailRepository.findAll().stream()
+                .map(this::mapToDTO)
+                .collect(Collectors.toList());
+    }
+    public DonationDetailDTO update(Long id, CreateDonationDetailDTO dto) {
+        DonationDetail donation = donationDetailRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Donation not found"));
+        donation.setDonAmount(dto.getDonAmount());
+        donation.setDonDate(dto.getDonDate());
+        donation.setBloodType(bloodTypeRepository.findById(dto.getBloodType())
+                .orElseThrow(() -> new EntityNotFoundException("Blood type not found")));
+        donation.setAppointment(appointmentRepository.findById(dto.getAppointmentId())
+                .orElseThrow(() -> new EntityNotFoundException("Appointment not found")));
+        donation.setMember((Member) memberRepository.findById(dto.getMemberId())
+                .orElseThrow(() -> new EntityNotFoundException("Member not found")));
+        donation.setStaff(staffRepository.findById(dto.getStaffId())
+                .orElseThrow(() -> new EntityNotFoundException("Staff not found")));
+        return mapToDTO(donationDetailRepository.save(donation));
+    }
+    public void delete(Long id) {
+        DonationDetail d = donationDetailRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Donation not found"));
+        donationDetailRepository.delete(d);
+    }
+    private DonationDetailDTO mapToDTO(DonationDetail detail) {
+        DonationDetailDTO dto = new DonationDetailDTO();
+        dto.setDonID(detail.getDonID());
+        dto.setDonAmount(detail.getDonAmount());
+        dto.setDonDate(detail.getDonDate());
+        dto.setBloodType(Long.valueOf(detail.getBloodType().getBloodType()));
+        dto.setAppointmentId(detail.getAppointment().getId());
+        dto.setMemberId(detail.getMember().getUserID());
+        dto.setStaffId(detail.getStaff().getUserID());
+        return dto;
+    }
+}
