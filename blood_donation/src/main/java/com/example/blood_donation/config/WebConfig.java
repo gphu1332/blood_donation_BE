a38b5fc7a61e011a4f613fed66f5f401f0558737
QuangKhanh34th <quangkhanhktdn@gmail.com>
2025-07-16 23:05:39 +0700

    update webConfig to accept request from hosted front-end

--- blood_donation/src/main/java/com/example/blood_donation/config/WebConfig.java
+++ blood_donation/src/main/java/com/example/blood_donation/config/WebConfig.java
@@ -13,7 +13,8 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins("http://localhost:5173",
+                                "https://blood-donation-lovat.vercel.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
