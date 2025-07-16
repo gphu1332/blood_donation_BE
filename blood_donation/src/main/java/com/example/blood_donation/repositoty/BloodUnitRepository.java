31d91a6f734ffe649636eabc09fd20e695ad5795
NguyenThiXuanGiang <nguyenxuangiangmt@gmail.com>
2025-07-16 16:53:32 +0700

    fix-validation-blood-unit

--- blood_donation/src/main/java/com/example/blood_donation/repositoty/BloodUnitRepository.java
+++ blood_donation/src/main/java/com/example/blood_donation/repositoty/BloodUnitRepository.java
@@ -1,10 +1,11 @@
 package com.example.blood_donation.repositoty;
 
 import com.example.blood_donation.entity.BloodUnit;
+import com.example.blood_donation.enums.TypeBlood;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface BloodUnitRepository extends JpaRepository<BloodUnit, Long> {
-    List<BloodUnit> findByBloodType_BloodType(String bloodType);
+    List<BloodUnit> findByTypeBlood(TypeBlood typeBlood);
 }
