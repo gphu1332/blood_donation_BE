7f43878867977d313315c00be7f80cd1207977d0
gphu1332 <gphu133@gmail.com>
2025-07-19 19:04:06 +0700

    fix bug

--- blood_donation/src/main/java/com/example/blood_donation/repository/AppointmentRepository.java
+++ blood_donation/src/main/java/com/example/blood_donation/repository/AppointmentRepository.java
@@ -14,31 +14,35 @@ public interface AppointmentRepository extends JpaRepository<Appointment, Long> 
     List<Appointment> findAppointmentsForProgramDate(@Param("date") LocalDate date);
 
     @Query("""
-        SELECT FUNCTION('DATE_FORMAT', a.date, '%Y-%m') as month,
-               a.status,
-               COUNT(a)
-        FROM Appointment a
-        WHERE a.date BETWEEN :startDate AND :endDate
-        GROUP BY FUNCTION('DATE_FORMAT', a.date, '%Y-%m'), a.status
-    """)
+                SELECT FUNCTION('DATE_FORMAT', a.date, '%Y-%m') as month,
+                       a.status,
+                       COUNT(a)
+                FROM Appointment a
+                WHERE a.date BETWEEN :startDate AND :endDate
+                GROUP BY FUNCTION('DATE_FORMAT', a.date, '%Y-%m'), a.status
+            """)
     List<Object[]> countByMonthAndStatus(@Param("startDate") LocalDate startDate,
                                          @Param("endDate") LocalDate endDate);
 
     @Query("""
-        SELECT a.program.proName, COUNT(a.id)
-        FROM Appointment a
-        GROUP BY a.program.id, a.program.proName
-        ORDER BY COUNT(a.id) DESC
-    """)
+                SELECT a.program.proName, COUNT(a.id)
+                FROM Appointment a
+                WHERE a.status = com.example.blood_donation.enums.Status.FULFILLED
+                GROUP BY a.program.id, a.program.proName
+                ORDER BY COUNT(a.id) DESC
+            """)
     List<Object[]> findTop10Programs();
 
+
     @Query("""
-        SELECT a.user.fullName, COUNT(a.id)
-        FROM Appointment a
-        GROUP BY a.user.id, a.user.fullName
-        ORDER BY COUNT(a.id) DESC
-    """)
+                SELECT a.user.fullName, COUNT(a.id)
+                FROM Appointment a
+                WHERE a.status = com.example.blood_donation.enums.Status.FULFILLED
+                GROUP BY a.user.id, a.user.fullName
+                ORDER BY COUNT(a.id) DESC
+            """)
     List<Object[]> findTop10Users();
+
 
     long count();
 
