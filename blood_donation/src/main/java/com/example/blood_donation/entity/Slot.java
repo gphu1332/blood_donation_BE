51e29964ed50a32da6cb7a0153b6730fa5595cc3
gphu1332 <gphu133@gmail.com>
2025-07-30 22:06:45 +0700

    Update Slot.java

--- blood_donation/src/main/java/com/example/blood_donation/entity/Slot.java
+++ blood_donation/src/main/java/com/example/blood_donation/entity/Slot.java
@@ -14,6 +14,7 @@ import java.util.List;
 public class Slot {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "slotid")
     private Long slotID;
 
     private String label;
