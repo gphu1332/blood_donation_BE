e86d8178fc2dc5c95afdc379525dd532f3a7f40d
gphu1332 <gphu133@gmail.com>
2025-08-02 15:21:18 +0700

    fix bug

--- blood_donation/src/main/java/com/example/blood_donation/repository/BloodRequestDetailRepository.java
+++ blood_donation/src/main/java/com/example/blood_donation/repository/BloodRequestDetailRepository.java
@@ -1,20 +1,11 @@
 package com.example.blood_donation.repository;
 
+
 import com.example.blood_donation.entity.BloodRequestDetail;
-import com.example.blood_donation.entity.BloodRequestDetailId;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
-public interface BloodRequestDetailRepository extends JpaRepository<BloodRequestDetail, BloodRequestDetailId> {
-    List<BloodRequestDetail> findByBloodRequest_ReqID(Long reqId);
-
-    @Modifying
-    @Query("DELETE FROM BloodRequestDetail d WHERE d.bloodRequest.reqID = :reqId")
-    void deleteAllByRequestId(@Param("reqId") Long reqId);
-
+public interface BloodRequestDetailRepository extends JpaRepository<BloodRequestDetail, Long> {
+    List<BloodRequestDetail> findByBloodRequest_ReqID(Long reqID);
 }
-
